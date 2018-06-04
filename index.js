46a2a6c6e35afb68561c52ac2880070c3ec58e41
Muhammad Orabi <muhammad.orabi.2014@hotmail.com>
2018-06-05 01:14:27 +0200

    init chatbot

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('bpdy-parser');
+const request = require('request');
+
+const app = express();
+app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.urlencode({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/webhock', function (req, res) {
+    if (req.query['hub.verify_token'] === 'facebookchatbot') {
+        res.send(req.query['hub.challenge']);
+    }
+    res.send('Wrong token');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('running: port');
+});
